fa7144bfdf2dab03840703a38c0d4e3ab5aba6ef
tnguyen606-cs <tnguyen606@gatech.edu>
2022-09-05 16:05:54 -0400

    Added Sign Up Screen

---
+++ MyDacoApp/app/src/main/java/com/example/daco/SignUpActivity.java
@@ -0,0 +1,32 @@
+package com.example.daco;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_screen);
+
+        Button regBtn = (Button) findViewById(R.id.registerBtn);
+
+        // continue
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toastMessage = "A new user is just created";
+                Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT).show();
+                Intent nextScreen = new Intent(SignUpActivity.this, MainActivity.class);
+                SignUpActivity.this.startActivity(nextScreen);
+            }
+        });
+
+    }
+}
