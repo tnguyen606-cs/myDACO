c7dfe95a5152c02cfc3416a4d55c1e11eb86f3ff
brentnguyen-io <brentnguyen.io@gmail.com>
2022-10-07 10:47:31 -0400

    Updated MainActivity.java w/Firebase Authentication
    - Replaced MainActivity.java with Firebase's Email Login and Google Login

--- MyDacoApp/app/src/main/java/com/myDACO/MainActivity.java
+++ MyDacoApp/app/src/main/java/com/myDACO/MainActivity.java
@@ -13,68 +13,186 @@ import com.myDACO.utilities.FileHelper;
 import com.google.android.material.button.MaterialButton;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Set;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.firebase.ui.auth.util.ExtraConstants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    // [START auth_fui_create_launcher]
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+    // [END auth_fui_create_launcher]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createSignInIntent();
         setContentView(R.layout.activity_main);
+    }
 
-        EditText usernameInput = (EditText) findViewById(R.id.usernamebox);
-        EditText passwordInput = (EditText) findViewById(R.id.passwordbox);
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+              //  new AuthUI.IdpConfig.FacebookBuilder().build(),
+             );
 
-        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
-        Button forgotBtn = (Button) findViewById(R.id.forgotBtn);
-        Button newUsersBtn = (Button) findViewById(R.id.newUsersBtn);
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_create_intent]
+    }
 
-        InputStream inputStream = getResources().openRawResource(R.raw.user_data);
+    // [START auth_fui_result]
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+    // [END auth_fui_result]
 
-        // admin Login
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FileHelper file = new FileHelper();
-                Set<Users> strUsersArray = file.getUserResource(inputStream);
-
-                String username = usernameInput.getText().toString();
-                String password = passwordInput.getText().toString();
-
-                boolean validUser = false;
-
-                for (Users u : strUsersArray) {
-                    if (u.getDacoUsername().equals(username) && u.getDacoPassword().equals(password)) {
-                        validUser = true;
-                        // Go to Plane Screen if valid login
-                        Intent nextScreen = new Intent(MainActivity.this, PlanesActivity.class);
-                        MainActivity.this.startActivity(nextScreen);
-                        break;
+    public void signOut() {
+        // [START auth_fui_signout]
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
                     }
-                }
-               if (!validUser) {
-                   String toastMessage = "Username or Password are not correct. Please enter the correct one";
-                   Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_LONG).show();
-               }
-            }
-        });
+                });
+        // [END auth_fui_signout]
+    }
 
-        // forgot Password
-        forgotBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent nextScreen = new Intent(MainActivity.this, ForgotPasswordActivity.class);
-                MainActivity.this.startActivity(nextScreen);
-            }
-        });
+    public void delete() {
+        // [START auth_fui_delete]
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // ...
+                    }
+                });
+        // [END auth_fui_delete]
+    }
 
-        // sign up
-        newUsersBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent nextScreen = new Intent(MainActivity.this, SignUpActivity.class);
-                MainActivity.this.startActivity(nextScreen);
+    public void themeAndLogo() {
+        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+
+        // [START auth_fui_theme_logo]
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+               // .setLogo(R.drawable.my_great_logo)      // Set logo drawable
+                //.setTheme(R.style.MySuperAppTheme)      // Set theme
+                .build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_theme_logo]
+    }
+
+    public void privacyAndTerms() {
+        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+
+        // [START auth_fui_pp_tos]
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                        "https://example.com/terms.html",
+                        "https://example.com/privacy.html")
+                .build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_pp_tos]
+    }
+
+    public void emailLink() {
+        // [START auth_fui_email_link]
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+                .setAndroidPackageName(
+                        /* yourPackageName= */ "...",
+                        /* installIfNotAvailable= */ true,
+                        /* minimumVersion= */ null)
+                .setHandleCodeInApp(true) // This must be set to true
+                .setUrl("https://google.com") // This URL needs to be whitelisted
+                .build();
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder()
+                        .enableEmailLinkSignIn()
+                        .setActionCodeSettings(actionCodeSettings)
+                        .build()
+        );
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+        // [END auth_fui_email_link]
+    }
+
+    public void catchEmailLink() {
+        List<AuthUI.IdpConfig> providers = Collections.emptyList();
+
+        // [START auth_fui_email_link_catch]
+        if (AuthUI.canHandleIntent(getIntent())) {
+            if (getIntent().getExtras() == null) {
+                return;
             }
-        });
+            String link = getIntent().getExtras().getString("email_link_sign_in");
+            if (link != null) {
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setEmailLink(link)
+                        .setAvailableProviders(providers)
+                        .build();
+                signInLauncher.launch(signInIntent);
+            }
+        }
+        // [END auth_fui_email_link_catch]
     }
 }
