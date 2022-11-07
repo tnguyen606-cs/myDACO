f50cd1747325e76aa4aaaae5f26c93cf066e5bf8
leng7 <71042228+leng916@users.noreply.github.com>
2022-11-07 11:20:46 -0500

    - Added menu functions to AddCargoActivity.java

--- MyDacoApp/app/src/main/java/com/myDACO/AddCargoActivity.java
+++ MyDacoApp/app/src/main/java/com/myDACO/AddCargoActivity.java
@@ -1,14 +1,67 @@
 package com.myDACO;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.myDACO.data.Planes;
+import com.myDACO.utilities.FileHelper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class AddCargoActivity extends AppCompatActivity {
+
+    private ArrayList<Planes> planesList = new ArrayList<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    ArrayAdapter<Planes> adapter;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //listens for changes to the firestore databases in real time
+        ListenerRegistration planeListener = db.collection("planes").addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.w("snapshot listener", "listen failed", error);
+                    return;
+                }
+                planesList.clear();
+                for (QueryDocumentSnapshot document : value) {
+                    Planes plane = document.toObject(Planes.class);
+                    planesList.add(plane);
+                    adapter.notifyDataSetChanged();
+                }
+                Collections.sort(planesList, new Comparator<Planes>() {
+                    public int compare(Planes p1, Planes p2) {
+                        return p1.getPlaneName().compareTo(p2.getPlaneName());
+                    }
+                });
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_cargo);
+
+        // User clicks on the menu bar to sign out action
+        FileHelper file = new FileHelper();
+        ImageView menuIcon = (ImageView) findViewById(R.id.menu_icon);
+        file.showMenu(AddCargoActivity.this, menuIcon);
     }
 }
