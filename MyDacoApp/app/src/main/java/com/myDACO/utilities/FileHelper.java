2e513594308d11b3fdaff371532657ce5b33fdf2
tnguyen606-cs <tnguyen606@gatech.edu>
2022-11-30 03:59:16 -0500

    Fixed context

--- MyDacoApp/app/src/main/java/com/myDACO/utilities/FileHelper.java
+++ MyDacoApp/app/src/main/java/com/myDACO/utilities/FileHelper.java
@@ -16,7 +16,6 @@ import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -28,16 +27,16 @@ import com.myDACO.PlanesActivity;
 import com.myDACO.MainActivity;
 import com.myDACO.R;
 
-public class FileHelper extends AppCompatActivity {
+public class FileHelper {
 
     private boolean isNotchecked = true;
-    private Activity thisContext;
+//    private Activity context;
 
-    public void showMenu(Activity context, ImageView menuIcon) {
+    public void showMenu(Activity ctx, ImageView menuIcon) {
         menuIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(context, view);
+                PopupMenu popupMenu = new PopupMenu(ctx, view);
                 popupMenu.getMenuInflater().inflate(R.menu.page_menu, popupMenu.getMenu());
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
@@ -45,34 +44,34 @@ public class FileHelper extends AppCompatActivity {
                         switch (menuItem.getItemId()) {
 
                             case R.id.cargo_item:
-                                Intent intent1 = new Intent(context, ListOfCargosActivity.class);
-                                context.startActivity(intent1);
+                                Intent intent1 = new Intent(ctx, ListOfCargosActivity.class);
+                                ctx.startActivity(intent1);
                                 break;
 
                             case R.id.personnel_item:
-                                Intent intent2 = new Intent(context, ListOfPersonnelActivity.class);
-                                context.startActivity(intent2);
+                                Intent intent2 = new Intent(ctx, ListOfPersonnelActivity.class);
+                                ctx.startActivity(intent2);
                                 break;
                                 
                             case R.id.planes_manifest_item:
-                                Intent intent3 = new Intent(context, PlanesActivity.class);
-                                context.startActivity(intent3);
+                                Intent intent3 = new Intent(ctx, PlanesActivity.class);
+                                ctx.startActivity(intent3);
                                 break;
 
                             case R.id.mission_manifest_item:
-                                Intent intent4 = new Intent(context, MissionActivity.class);
-                                context.startActivity(intent4);
+                                Intent intent4 = new Intent(ctx, MissionActivity.class);
+                                ctx.startActivity(intent4);
                                 break;
 
                             case R.id.excel_item:
-                                showCustomDialog(context);
+                                showCustomDialog(ctx);
                                 break;
 
                             case R.id.sign_out_item:
                                 FirebaseAuth myAuth = FirebaseAuth.getInstance();
                                 myAuth.signOut();
-                                Intent intent = new Intent(context, MainActivity.class);
-                                context.startActivity(intent);
+                                Intent intent = new Intent(ctx, MainActivity.class);
+                                ctx.startActivity(intent);
                                 break;
 
                             default:
@@ -86,8 +85,8 @@ public class FileHelper extends AppCompatActivity {
         });
     }
 
-    void showCustomDialog(Activity context) {
-        Dialog dialog = new Dialog(context);
+    void showCustomDialog(Activity ctx) {
+        Dialog dialog = new Dialog(ctx);
         //We have added a title in the custom layout. So let's disable the default title.
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //The user will be able to cancel the dialog by clicking anywhere outside the dialog.
@@ -116,17 +115,22 @@ public class FileHelper extends AppCompatActivity {
             public void onClick(View view) {
 //                Log.d("Print:   ", "Plane");
                 if (plane.isChecked()) {
-                    Toast.makeText(context.getApplicationContext(), "Excelled Planes to Firebase", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ctx.getApplicationContext(), "Excelled Planes to Firebase", Toast.LENGTH_LONG).show();
                 } else if (cargo.isChecked()) {
-                    Toast.makeText(context.getApplicationContext(), "Excelled Cargos to Firebase", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ctx.getApplicationContext(), "Excelled Cargos to Firebase", Toast.LENGTH_LONG).show();
                 } else if (personnel.isChecked()) {
-                    Toast.makeText(context.getApplicationContext(), "Excelled Personnel to Firebase", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ctx.getApplicationContext(), "Excelled Personnel to Firebase", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(context.getApplicationContext(), "Please make sure you checked one of the box!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ctx.getApplicationContext(), "Please make sure you checked one of the box!", Toast.LENGTH_LONG).show();
                     isNotchecked = false;
                 }
                 if (isNotchecked) {
-                    importExcelFile(context);
+//                    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//                        selectfile();
+//                    } else {
+//                        ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 101);
+//                        Log.w("Print:   ", "Permission Not granted plaoajnajnd");
+//                    }
                     dialog.dismiss();
                 }
             }
@@ -134,36 +138,29 @@ public class FileHelper extends AppCompatActivity {
         dialog.show();
     }
 
-    private void importExcelFile(Activity context) {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            selectfile(context);
-        } else {
-            ActivityCompat.requestPermissions(context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 101);
-        }
-    }
-
 //    // request for storage permission if not given
 //    @Override
 //    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 //        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        Log.w("Print:   ",  "plaoajnajnd");
 //        if (requestCode == 101) {
 //            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                selectfile();
+//                Log.w("Print:   ", "Permission granted");
 //            } else {
-//                Log.d("Print:   ", "Permission Not granted");
+//                Log.w("Print:   ", "Permission Not granted");
 ////                Toast.makeText(thisContext, "Permission Not granted", Toast.LENGTH_LONG).show();
 //            }
 //        }
 //    }
-//
-    private void selectfile(Activity context) {
+
+    private void selectfile() {
         // select the file from the file storage
-        Intent chooseFile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+//        Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
 //        chooseFile.addCategory(Intent.CATEGORY_OPENABLE);
-        chooseFile.setType("*/*");
-        Intent intentFile = Intent.createChooser(chooseFile, "Choose a file");
-        context.startActivityForResult(intentFile, 102);
-        Log.w("Print:   ", "Permission Not granted");
+//        chooseFile.setType("*/*");
+//        Intent intentFile = Intent.createChooser(chooseFile, "Choose a file");
+//        context.startActivityForResult(intentFile, 102);
+        Log.w("Print:   ", "Select File");
     }
 
 //    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
